475a84b06659d8033bd180adb83d66057ff1b8ec
avaniawang <avaniawang@outlook.com>
2024-07-12 14:26:37 +0800

    add some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+/*!
+A Library for interacting with the League of Legends Client API and the League of Legends Model.
+
+# Interacting with LCU (League Client Update)
+``` rust
+pub async fn test() -> anyhow::Result<()> {
+    let lcu_client = lcu_api::LcuClient::new();
+    let histories = lcu_client.get_summoner_match_histories().await?;
+    println!("{:?}", histories);
+    Ok(())
+}
+```
+*/
 pub use league_model::*;
 pub use lcu_api::*;
 pub use client_api::*;
